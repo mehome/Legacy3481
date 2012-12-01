b7872fc49fb1a518fd6dc8b2840d2605635dad01
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2012-12-01 23:06:24 +0000

    No code change... corrected the comments on the color space info

--- BroncBotz_DashBoard/Source/ProcessingVision/ProcessingVision.h
+++ BroncBotz_DashBoard/Source/ProcessingVision/ProcessingVision.h
@@ -18,7 +18,9 @@ struct Bitmap_Frame
 /// \ret This gives the ability to control what gets shown on the video feed (i.e. preview) This makes it possible to make composite overlaying 
 /// (e.g. targeting reticle or bound box feedback of acquired target).  You can return the same frame sent or create a new one that has
 /// different properties typically the stride may be different depending on the kind of buffers you need to allocate
-/// The format color space of the memory is RGB 24.  This does not change and must remain this for frames returned
+/// The format color space of the memory is BGRA 32.  This does not change and must remain this for frames returned
+/// \note The color space BGRA 32 has been chosen since this is what NI_Vision uses, we can however change this to some other format (e.g YUV 4.2.2)
+/// if necessary.  The m1011 camera in h264 mode produces YUV 420p by default, and this gets converted to desired colorspace.
 extern "C" PROCESSINGVISION_API Bitmap_Frame *ProcessFrame_RGB32(Bitmap_Frame *Frame);
 
 // redundant, but allows for easy separation.
