39b92e6f687a45e9914ff39bd669e2730a9f2b01
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2013-01-26 01:36:16 +0000

    After discovering the need to have a UDP_Listener running at all times to avoid flooding buffers.  This change puts the listener into its own task that is started on power up.

--- BroncBotz_DashBoard/Source/VisionSample2012/VisionSample2012.cpp
+++ BroncBotz_DashBoard/Source/VisionSample2012/VisionSample2012.cpp
@@ -25,12 +25,17 @@ unsigned long _byteswap_ulong(unsigned long i)
     return j;
 }
 
+class UDP_Listener;
+static void UDP_Listener_Receive_Task(UDP_Listener *instance);
+
 class UDP_Listener
 {
 	public: 
 	//http://www.chiefdelphi.com/forums/showthread.php?p=1024930
 	//has info about dashboard to robot using 1130 and robot to dashboard using 1140
-	UDP_Listener(UDP_Listener_Interface *client,int portno=1130) : m_Client(client),m_Error(false)
+	UDP_Listener(UDP_Listener_Interface *client,int portno=1130) : 
+		m_task ("UDP_Listener", (FUNCPTR)UDP_Listener_Receive_Task),
+		m_Client(client),m_Error(false),m_IsRunning(true)
 	{
 		try 
 		{
@@ -48,6 +53,9 @@ class UDP_Listener
 				 throw 1;
 			 listen(m_sockfd,5);
 			 clilen = sizeof(cli_addr);
+			//if we made it this far we can start the new task
+			if (!m_task.Start((INT32)this))
+				throw 2;
 		}
 		catch (int ErrorCode)
 		{
@@ -60,6 +68,9 @@ class UDP_Listener
 			case 1:
 				ErrorMsg="ERROR on binding";
 				break;
+			case 2:
+				ErrorMsg="ERROR on starting recieve task";
+				break;
 			};
 			if (ErrorMsg)
 				printf("ErrorMsg=%s\n",ErrorMsg);
@@ -68,9 +79,10 @@ class UDP_Listener
 	}
 	~UDP_Listener()
 	{
+		m_IsRunning=false;
 	     close(m_sockfd);
 	}
-	void TimeChange(double dTime_s)
+	void TimeChange()
 	{
 		if (m_Error) return;
 	     char buffer[256];
@@ -78,31 +90,70 @@ class UDP_Listener
 	     struct sockaddr_in fromAddr; 
 	     int fromSize = sizeof(fromAddr);
    		 int n=recvfrom(m_sockfd, buffer, 255, 0, (struct sockaddr *) &fromAddr, &fromSize);
-	     
+
+   		 //Note: typically this would fail if the ioctl(m_sockfd, FIONBIO,(int) &mode) mode was set to 1, but since this is in its own thread recvfrom() can use its
+   		 //internal blocking (i.e. critical section/mutex) and generally return successful
 	     if (n>0)
 	     {
 	    	 m_Client->ProcessPacket(buffer,n);
 	     }
+	     else
+	 		Wait(0.005);  //avoid busy wait
 	}
+	
+	bool IsRunning() const {return m_IsRunning;}
+
 	private:
+	Task m_task;
 	UDP_Listener_Interface *m_Client; //delegate packet to client code
 	int m_sockfd;
 	bool m_Error;
+	bool m_IsRunning;
 };
+
+//I generally do not recommend using threads at all for robot code, but we have a case that we cannot control.  Currently VxWorks 6.3 has a problem where if UDP packets are sent without
+//a listener to empty them the buffers can overflow and flood into legit TCP/IP packets, and cause repeated loss of communication with the driver station.  Therefore as soon as the robot
+//powers on this thread needs to start up immediately, and it will continue to ensure all packets are received.  It can also do so using its own internal blocking 
+//(i.e. critical section / mutex) intact.  This means that it will not busy wait anytime, and for the rare case of an error we will sleep for 5 ms. 
+static void UDP_Listener_Receive_Task(UDP_Listener *instance)
+{
+	while (instance->IsRunning())
+	{
+		instance->TimeChange();  //handles sleep implicitly
+	}
+}
 
 class coodinate_manager : public UDP_Listener_Interface
 {
+	protected:
+		//You could expose these instead to public
+		__inline double GetXpos() const {return m_Xpos;}
+		__inline double GetYpos() const {return m_Ypos;}
+		__inline bool IsUpdated() const {return m_Updated;}
+		void ResetUpdate() {m_Updated=false;}
 	public:
 		coodinate_manager() : m_UDP(this)
 		{
 			
 		}
+
+		//This demonstrates how to interface with the listener for updates without needing critical sections by use of the bool... using bool is an automic operation where there
+		//can never be any real danger of a race condition, the worse that can happen is that the update happens on the next iteration, but in our case the thread runs every 33ms
+		//while our time change runs about every 10ms, so it is a non-issue
 		void TimeChange(double dTime_s)
 		{
-			m_UDP.TimeChange(dTime_s);
-			//TODO smart dashboard display and servo manipulation
+			if (IsUpdated())
+			{
+				ResetUpdate();
+				//This shows how well the coordinate notifications stay in sync with the UDP_Listener thread updates
+				SmartDashboard::PutNumber("Auton X",GetXpos());
+				SmartDashboard::PutNumber("Auton Y",GetYpos());
+			}
 		}
 	protected: //from UDP_Listener_Interface
+		
+		// This is call from within the UDP_Listener task/thread.  The convergence between this thread and client code can happen with need of any blocking by use of an atomic operation 
+		//of the bool (think of it as volatile, but simple enough logic to not warrent the need for declaration).  
 		virtual void ProcessPacket(char *pkt,size_t pkt_size)
 		{
 			if (pkt_size==16)
@@ -117,6 +168,7 @@ class coodinate_manager : public UDP_Listener_Interface
 				{
 					if (checksum==XInt+YInt)
 					{
+						m_Updated=true;
 						m_Xpos=(double)XInt / 10000000.0;
 						m_Ypos=(double)YInt / 10000000.0;
 						//printf("New coordinates %f , %f\n",m_Xpos,m_Ypos);
@@ -131,19 +183,21 @@ class coodinate_manager : public UDP_Listener_Interface
 				printf("warning packet size=%d\n",pkt_size);
 		}
 	private:
-	 UDP_Listener m_UDP;
-	 double m_Xpos,m_Ypos;
+		UDP_Listener m_UDP;
+		double m_Xpos,m_Ypos;
+		bool m_Updated; //true if we received a packet for this slice of time
 };
 
 class VisionSample2012 : public SimpleRobot
 {
+	coodinate_manager app;  //This (i.e. UDP listenter thread) needs to start immediately!!
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
-	AxisCamera *camera;
 
 public:
 	VisionSample2012(void):
-		myRobot(1, 2),	// these must be initialized in the same order
+		//Note I've put these in a higher channel since I've been testing with a kit that uses 1 and 2 for servos
+		myRobot(3, 4),	// these must be initialized in the same order
 		stick(1)		// as they are declared above.
 	{
 		myRobot.SetExpiration(0.1);
@@ -155,7 +209,6 @@ public:
 	 */
 	void Autonomous(void)
 	{
-		coodinate_manager app;
 		double tm = GetTime();
 		while (IsAutonomous() && !IsDisabled())
 		{
