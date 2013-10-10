3d03b1b0bd2605b8e127caf904f3f2273872a41c
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2013-10-10 22:40:23 +0000

    Temporary lua save file

---
+++ Temp/CompositorSave.lua
@@ -0,0 +1,7 @@
+sequence_load =
+{
+	sequence_1 = {	type="square", x=0.5, y=0.5 },
+	sequence_2 = {	type="square", x=0.0, y=0.0 },
+	sequence_3 = {	type="square", x=0.0, y=0.0 },
+	sequence_4 = {	type="none", x=0.0, y=0.0 }
+}
