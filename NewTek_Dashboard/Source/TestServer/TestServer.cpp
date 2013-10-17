dafa9861f9c20b2a791b0f37f6a754048674805b
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2013-10-17 11:23:21 +0000

    Added a quick test server for demo

---
+++ NewTek_Dashboard/Source/TestServer/TestServer.cpp
@@ -0,0 +1,113 @@
+// TestServer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "../SmartDashboard/SmartDashboard_import.h"
+
+using namespace std;
+
+/************************************************************************
+* FUNCTION: cls(HANDLE hConsole)                                        * 
+*                                                                       * 
+* PURPOSE: clear the screen by filling it with blanks, then home cursor * 
+*                                                                       * 
+* INPUT: the console buffer to clear                                    * 
+*                                                                       * 
+* RETURNS: none                                                         * 
+*************************************************************************/  
+void cls(HANDLE hConsole=NULL) 
+{   
+	if (!hConsole)
+		hConsole=::GetStdHandle( STD_OUTPUT_HANDLE );
+	COORD coordScreen = { 0, 0 }; /* here's where we'll home the cursor */   
+	BOOL bSuccess;   
+	DWORD cCharsWritten;   
+	CONSOLE_SCREEN_BUFFER_INFO csbi; /* to get buffer info */   
+	DWORD dwConSize; /* number of character cells in the current buffer */    
+	/* get the number of character cells in the current buffer */   
+	bSuccess = GetConsoleScreenBufferInfo(hConsole, &csbi);   
+	//PERR(bSuccess, "GetConsoleScreenBufferInfo");   
+	dwConSize = csbi.dwSize.X * csbi.dwSize.Y;   /* fill the entire screen with blanks */   
+	bSuccess = FillConsoleOutputCharacter(hConsole, (TCHAR) ' ',       
+		dwConSize, coordScreen, &cCharsWritten);   
+	//PERR(bSuccess, "FillConsoleOutputCharacter");   /* get the current text attribute */   
+	bSuccess = GetConsoleScreenBufferInfo(hConsole, &csbi);   
+	//PERR(bSuccess, "ConsoleScreenBufferInfo");   /* now set the buffer's attributes accordingly */   
+	bSuccess = FillConsoleOutputAttribute(hConsole, csbi.wAttributes,       dwConSize, coordScreen, &cCharsWritten);   
+	//PERR(bSuccess, "FillConsoleOutputAttribute");   /* put the cursor at (0, 0) */   
+	bSuccess = SetConsoleCursorPosition(hConsole, coordScreen);   
+	//PERR(bSuccess, "SetConsoleCursorPosition");   return; 
+} 
+
+void DisplayHelp()
+{
+	printf(
+		"Sequence <#>\n"
+		"Edit <off on>"
+		"Help (displays this)\n"
+		"\nType \"Quit\" at anytime to exit this application\n"
+		);
+}
+
+#pragma warning(disable : 4996)
+void CommandLineInterface()
+{
+	char input_line[128];
+
+	char		command[32];
+	char		str_1[MAX_PATH];
+	char		str_2[MAX_PATH];
+	char		str_3[MAX_PATH];
+	char		str_4[MAX_PATH];
+	char		str_5[MAX_PATH];
+
+	const char * const Args[]=
+	{
+		str_1,str_2,str_3,str_4,str_5
+	};
+
+	while (cout << ">",cin.getline(input_line,128))
+	{
+		command[0]=0;
+		str_1[0]=0;
+		str_2[0]=0;
+		str_3[0]=0;
+		str_4[0]=0;
+		str_5[0]=0;
+
+		if (sscanf( input_line,"%s %s %s %s %s %s",command,str_1,str_2,str_3,str_4,str_5)>=1)
+		{
+			if (!_strnicmp( input_line, "cls", 3))
+			{
+				cls();
+			}
+			else if (!_strnicmp( input_line, "sequence", 3))  //or seq for short
+			{
+				SmartDashboard::PutNumber("Sequence",atof(str_1));
+			}
+			else if (!_strnicmp( input_line, "edit", 4)) 
+			{
+				bool Edit=false;
+				if (_stricmp("on",str_1)==0)
+					Edit=true;
+				else if (str_1[0]=='1' || str_1[0]=='y' || str_1[0]=='Y')
+					Edit=true;
+				SmartDashboard::PutBoolean("Edit Position",Edit);
+			}
+			else if (!_strnicmp( input_line, "Help", 4))
+				DisplayHelp();
+			else if (!_strnicmp( input_line, "Quit", 4))
+				break;
+			else
+				cout << "huh? - try \"help\"" << endl;
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	SmartDashboard::init();
+	DisplayHelp();
+	CommandLineInterface();
+	SmartDashboard::shutdown();
+}
