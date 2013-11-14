ae060a530823875a66f1afe6c4a19cd14a9d79e0
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2013-11-14 22:18:10 +0000

    Added preliminary draft of LUA init

---
+++ Temp/Video1.lua
@@ -0,0 +1,25 @@
+DashboardProps = 
+{
+	settings =
+	{
+		title= "Preview",
+		url= "10.28.1.20",
+		robot_ip_address= "localhost",
+		stream_profile= "default",
+		--TODO offload to the save file
+		left= 646,
+		top= 140,
+		right= 1150,
+		bottom= 518,
+		smart_dashboard= "java -jar C:\\WindRiver\\WPILib\\SmartDashboard.jar ip localhost",
+		window_name= "SmartDashboard",
+		is_popup= 0,
+		plug_in= "Compositor.dll",
+		aux_startup_file="",
+		aux_startup_file_args="",
+		record_frames= 0,
+		record_path= "D:\\media\\Robot_Capture\\"
+	},
+}
+
+Dashboard = DashboardProps
