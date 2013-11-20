3c301a9f82f125a0633a560bd71265d8a7dff5a0
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2013-11-20 05:26:41 +0000

    Added to example to load the persistence

--- Temp/Video1.lua
+++ Temp/Video1.lua
@@ -1,24 +1,39 @@
+--This is used load persistence of the coordinates last saved etc
+function GetSettings()
+	local loaded_file = loadfile("Video1Save.lua")
+	if (loaded_file==nil) then
+		return nil
+	else
+		loaded_file()  --We can now make the call and it will succeed
+		return settings_load
+	end
+end
+
 DashboardProps = 
 {
 	settings =
 	{
 		title= "Preview",
-		url= "10.28.1.20",
+		url= "E:/Media/Video/WMV/ReboundRedTest.wmv",
 		robot_ip_address= "localhost",
 		stream_profile= "default",
 		--TODO offload to the save file
-		left= 646,
-		top= 140,
-		right= 1150,
-		bottom= 518,
-		smart_dashboard= "java -jar C:\\WindRiver\\WPILib\\SmartDashboard.jar ip localhost",
+		left= 537,
+		top= 144,
+		right= 1216,
+		bottom= 653,
+		--smart_dashboard= "java -jar C:\\WindRiver\\WPILib\\SmartDashboard.jar ip localhost",
+		smart_dashboard="none",
 		window_name= "SmartDashboard",
 		is_popup= 0,
 		plug_in= "Compositor.dll",
 		aux_startup_file="",
 		aux_startup_file_args="",
 		record_frames= 0,
-		record_path= "D:\\media\\Robot_Capture\\"
+		record_path= "D:\\media\\Robot_Capture\\",
+		
+		--This must stay here to load the settings
+		load_settings = GetSettings(),
 	},
 }
 
---
+++ Temp/Video1Save.lua
@@ -0,0 +1,11 @@
+settings_load =
+{
+	url= "E:/Media/Video/WMV/ReboundRedTest.wmv",
+	left= 537,
+	top= 144,
+	right= 1216,
+	bottom= 653,
+	is_popup= 0,
+	record_frames= 0,
+	record_path= "D:\\media\\Robot_Capture\\"
+}
