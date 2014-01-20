64a3eeafd63fb253acda0b7903d5a59cb18e2b40
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2014-01-20 22:20:15 +0000

    Added example of convenient way to make dashboard updates

---
+++ 2014/2014Dashboard/Update.bat
@@ -0,0 +1,4 @@
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Release\Dashboard.exe /D /S /Y
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Release\*.dll /D /S /Y
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Release\Dashboard.exe D:\Stuff\Code\2014\2014Dashboard\RearCamera\ /D /S /Y
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Release\*.dll D:\Stuff\Code\2014\2014Dashboard\RearCamera\ /D /S /Y
