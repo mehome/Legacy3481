d0bfc55131014451e8e29d7b7b8d79d1376d5cc2
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2014-01-21 19:37:46 +0000

    Removed the RearView cleanup as this is no longer necessary

--- 2014/2014Dashboard/Clean.bat
+++ 2014/2014Dashboard/Clean.bat
@@ -1,4 +1,2 @@
 erase D:\Stuff\Code\2014\2014Dashboard\Dashboard.exe 
 erase D:\Stuff\Code\2014\2014Dashboard\*.dll /Q
-erase D:\Stuff\Code\2014\2014Dashboard\RearCamera\Dashboard.exe 
-erase D:\Stuff\Code\2014\2014Dashboard\RearCamera\*.dll /Q
