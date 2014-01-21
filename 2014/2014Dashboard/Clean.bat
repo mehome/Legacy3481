22089c6faeadeb67ea28f5431c01bfbbb3779ec7
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2014-01-21 03:14:33 +0000

    Added some other helpful batch files

---
+++ 2014/2014Dashboard/Clean.bat
@@ -0,0 +1,4 @@
+erase D:\Stuff\Code\2014\2014Dashboard\Dashboard.exe 
+erase D:\Stuff\Code\2014\2014Dashboard\*.dll /Q
+erase D:\Stuff\Code\2014\2014Dashboard\RearCamera\Dashboard.exe 
+erase D:\Stuff\Code\2014\2014Dashboard\RearCamera\*.dll /Q
---
+++ 2014/2014Dashboard/UpdateDebug.bat
@@ -0,0 +1,4 @@
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Debug\Dashboard.exe /D /S /Y
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Debug\*.dll /D /S /Y
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Debug\Dashboard.exe D:\Stuff\Code\2014\2014Dashboard\RearCamera\ /D /S /Y
+xcopy D:\Stuff\Code\NewTek_Dashboard\Bin\Debug\*.dll D:\Stuff\Code\2014\2014Dashboard\RearCamera\ /D /S /Y
