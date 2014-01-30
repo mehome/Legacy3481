71a97c1418561c1e6b0ae66a7f13486c6449498d
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2014-01-30 20:41:30 +0000

    This sets up a camera intake to see where to put camera

---
+++ 2013/OldCode/Compositor_RearView_CameraAdjust.lua
@@ -0,0 +1,126 @@
+Pi=3.14159265358979323846
+
+--This is used load persistence of the coordinates last saved etc
+function GetSettings()
+	local loaded_file = loadfile("CompositorSave_RearView.lua")
+	if (loaded_file==nil) then
+		return nil
+	else
+		loaded_file()  --We can now make the call and it will succeed
+		return sequence_load
+	end
+end
+
+CompositorProps = {
+	bypass_plugin="ProcessingVision.dll",
+	settings =
+	{
+		x_scalar=0.05,
+		y_scalar=0.05,
+		square_reticle_props =
+		{
+			square_reticle_1 =
+			{
+				thickness_x=6,thickness_y=6,opacity=0.7,
+				r=0,g=255,b=100,
+
+				use_shadow='y', exclude_region='y',
+				shadow =
+				{
+					thickness_x=6,thickness_y=6,opacity=0.9,
+					r=0,g=0,b=0
+				},
+				x_offset=2,y_offset=1
+			},
+		},
+		path_align =
+		{
+			width_in = 25, length_ft = 10,
+			pivot_in = 2,
+			camera_position =	{ x_in=0, y_in=12, z_in=-25 },
+			camera_rotation =	{ x=0, y_deg=0, z=0 },
+			r=0,g=255,b=100,
+			disable_motion = 'y',
+			rear_view='y',
+			fov=45,
+		},
+		
+		shape_reticle_props =
+		{
+			-- draw_shape -- may be "square", "circle", or "cube".
+			shape_reticle_1 =
+			{
+				remote_name="intake_reticle",
+				length_in=25,
+				width_in=25,
+				r=255,g=255,b=0,
+				rotation={x=0,y_deg=45,z_deg=0},
+				draw_shape="square"
+			},
+			shape_reticle_2 =
+			{
+				remote_name="land_reticle",
+				size_in=25,
+				r=250,g=0,b=0,
+				plane_selection="xy",
+				draw_shape="circle"
+			}
+		},
+
+		--Types	"none","square","composite","bypass"
+		sequence =
+		{
+			sequence_1 = 
+			{	
+				type="composite",
+				composite=
+				{
+					composite_1 = {	type="square", selection=1	},
+					composite_2 = {	type="pathalign" },
+					composite_3 = { type="shape", selection=1 },
+					composite_4 = { type="shape", selection=2 }
+				}
+			},
+			sequence_2 = {	type="bypass" },
+			--This must stay here to load the settings
+			load_settings = GetSettings(),
+		}
+	},
+	controls =
+	{
+		Joystick_1 =
+		{
+			control = "logitech dual action",
+			SetXAxis = {type="joystick_analog", key=0, is_flipped=false, multiplier=1.0, filter=0.3, curve_intensity=1.0},
+			SetYAxis = {type="joystick_analog", key=1, is_flipped=true, multiplier=1.0, filter=0.3, curve_intensity=0.0},
+			SetZAxis = {type="joystick_analog", key=5, is_flipped=true, multiplier=1.0, filter=0.3, curve_intensity=0.0},
+			SequencePOV =  {type="joystick_analog", key=8, is_flipped=false, multiplier=1.0, filter=0.0, curve_intensity=0.0},
+			ToggleLinePlot     = {type="joystick_button", key=6, on_off=false},
+			ResetPos     = {type="joystick_button", key=2, keyboard=' ', on_off=false},
+		},
+		Joystick_2 =
+		{
+			control = "gamepad f310 (controller)",
+			SetXAxis = {type="joystick_analog", key=0, is_flipped=false, multiplier=1.0, filter=0.3, curve_intensity=1.0},
+			SetYAxis = {type="joystick_analog", key=1, is_flipped=true, multiplier=1.0, filter=0.3, curve_intensity=0.0},
+			SequencePOV =  {type="joystick_analog", key=8, is_flipped=false, multiplier=1.0, filter=0.0, curve_intensity=0.0},
+			ToggleLinePlot     = {type="joystick_button", key=6, on_off=false},
+		},		
+		Joystick_3 =
+		{
+			control = "airflo",
+			SetXAxis = {type="joystick_analog", key=0, is_flipped=false, multiplier=1.0, filter=0.3, curve_intensity=1.0},
+			SetYAxis = {type="joystick_analog", key=1, is_flipped=true, multiplier=1.0, filter=0.3, curve_intensity=0.0},
+			--These sequence events happen whether or not you are in edit so choose what you want to use during the game
+			--The POV is not supported by the driver station so it is the perfect chose... I've added buttons here to show if you need to use them instead
+			--NextSequence     = {type="joystick_button", key=2, keyboard='y', on_off=false},
+			--PreviousSequence = {type="joystick_button", key=3, keyboard='y', on_off=false},
+			ResetPos     = {type="joystick_button", key=1, on_off=false},
+			ToggleLinePlot     = {type="joystick_button", key=6, on_off=false},
+			SequencePOV =  {type="joystick_analog", key=8, is_flipped=false, multiplier=1.0, filter=0.0, curve_intensity=0.0},
+		},
+
+	},
+}
+
+Compositor = CompositorProps
