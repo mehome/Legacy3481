01842675144117564e0217a6e3ae746d020dc59d
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2015-03-09 16:37:32 +0000

    Tweaked several settings to drive based from 2013 and 2014 code review... added back linear gain assist.

--- lua/FRC2015/Competition/Release Version/FRC2015Robot.lua
+++ lua/FRC2015/Competition/Release Version/FRC2015Robot.lua
@@ -31,7 +31,8 @@ skid=1
 gMaxTorqueYaw = (2 * Drive_MaxAccel * Meters2Inches / WheelTurningDiameter_In) * skid
 
 MainRobot = {
-	version = 1.1;
+	--Version helps to identify a positive update to lua
+	version = 1.2;
 	control_assignments =
 	{
 		--by default module is 1, so only really need it for 2
@@ -77,13 +78,11 @@ MainRobot = {
 		},
 		compressor	=	{ relay=8, limit=14 }
 	},
-	--Version helps to identify a positive update to lua
-	--version = 1;
 	
 	Mass = 25, -- Weight kg
-	MaxAccelLeft = 20, MaxAccelRight = 20, 
+	MaxAccelLeft = 10, MaxAccelRight = 10, 
 	MaxAccelForward = Drive_MaxAccel, MaxAccelReverse = Drive_MaxAccel, 
-	MaxAccelForward_High = Drive_MaxAccel * 2, MaxAccelReverse_High = Drive_MaxAccel * 2, 
+	MaxAccelForward_High = Drive_MaxAccel, MaxAccelReverse_High = Drive_MaxAccel, 
 	MaxTorqueYaw =  gMaxTorqueYaw,  --Note Bradley had 0.78 reduction to get the feel
 	MaxTorqueYaw_High = gMaxTorqueYaw * 5,
 	MaxTorqueYaw_SetPoint = gMaxTorqueYaw * 2,
@@ -119,7 +118,8 @@ MainRobot = {
 		latency=0.0,
 		heading_latency=0.0,
 		drive_to_scale=0.50,				--For 4 to 10 50% gives a 5 inch tolerance
-		left_max_offset=0.0 , right_max_offset=0.0,   --Ensure both tread top speeds are aligned
+		--left_max_offset=-0.7 , right_max_offset=0.0,   --Ensure both tread top speeds are aligned
+		left_max_offset=0.0 , right_max_offset=0.0,
 		--This is obtainer from encoder RPM's of 1069.2 and Wheel RPM's 427.68 (both high and low have same ratio)
 		encoder_to_wheel_ratio=0.5,			--example if encoder spins at 1069.2 multiply by this to get 427.68 (for the wheel rpm)
 		voltage_multiply=1.0,				--May be reversed using -1.0
@@ -132,10 +132,11 @@ MainRobot = {
 		 left_encoder_reversed='no',
 		right_encoder_reversed='no',
 		inv_max_accel = 1/15.0,  --solved empiracally
-		forward_deadzone_left  = 0.02,
-		forward_deadzone_right = 0.02,
-		reverse_deadzone_left  = 0.02,
-		reverse_deadzone_right = 0.02,
+		linear_gain_assist = 0.03,
+		--forward_deadzone_left  = 0.02,
+		--forward_deadzone_right = 0.02,
+		--reverse_deadzone_left  = 0.02,
+		--reverse_deadzone_right = 0.02,
 		motor_specs =
 		{
 			wheel_mass=1.5,
@@ -270,32 +271,7 @@ MainRobot = {
 				{p=25, i=0, d=5},
 				right_pid=
 				{p=25, i=0, d=5},					--These should always match, but able to be made different
-				--latency=0.300,
-				--I'm explicitly keeping this here to show that we have the same ratio (it is conceivable that this would not always be true)
-				--This is obtainer from encoder RPM's of 1069.2 and Wheel RPM's 427.68 (both high and low have same ratio)
-				encoder_to_wheel_ratio=0.5,			--example if encoder spins at 1069.2 multiply by this to get 427.68 (for the wheel rpm)
 				voltage_multiply=1.0,				--May be reversed using -1.0
-				--Note: this is only used in simulation as 884 victors were phased out, but encoder simulators still use it
-			--	curve_voltage=
-			--	{t4=3.1199, t3=-4.4664, t2=2.2378, t1=0.1222, c=0},
-				reverse_steering='no',
-				 left_encoder_reversed='no',
-				right_encoder_reversed='no',
-				inv_max_accel = 1/15,  --solved empiracally
-				motor_specs =
-				{
-					wheel_mass=1.5,
-					cof_efficiency=1.0,
-					gear_reduction=5310.0/346.6368,
-					torque_on_wheel_radius=Inches2Meters * 1,
-					drive_wheel_radius=Inches2Meters * 2,
-					number_of_motors=2,
-					
-					free_speed_rpm=5310.0,
-					stall_torque=2.43,
-					stall_current_amp=133,
-					free_current_amp=2.7
-				}
 			}
 		}
 	},
