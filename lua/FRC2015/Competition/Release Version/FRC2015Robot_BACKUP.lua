744d99f40485c11726995bbd28cacd6d06994a4f
ryan <ryan@4c771cc3-0104-4cfc-9633-31de2bc38340>
2015-03-12 20:23:44 +0000

    Current release of lua, including a failsafe copy with default values that are known to work. README.txt explains each lua's contents.

--- lua/FRC2015/Competition/Release Version/FRC2015Robot_BACKUP.lua
+++ lua/FRC2015/Competition/Release Version/FRC2015Robot_BACKUP.lua
@@ -1,3 +1,5 @@
+--This lua has orginal values
+
 Pi=3.14159265358979323846
 Pi2=Pi*2
 Inches2Meters=0.0254
@@ -300,8 +302,8 @@ MainRobot = {
 			--Joystick_SetRightVelocity = {type="joystick_analog", key=5, is_flipped=true, multiplier=1.0, filter=0.1, curve_intensity=3.0},
 			--Joystick_SetRight_XAxis = {type="joystick_analog", key=5, is_flipped=false, multiplier=1.0, filter=0.1, curve_intensity=1.0},			             		--Analog_Turn = {type="joystick_culver", key_x=5, key_y=2, is_flipped=false, multiplier=1.0, filter=0.3, curve_intensity=1.0},
 			
-			Analog_Turn = {type="joystick_analog", key=4, is_flipped=false, multiplier=0.55, filter=0.05, curve_intensity=1.0},
-			KickerWheel_SetCurrentVelocity = {type="joystick_analog", key=0, is_flipped=false, multiplier=.65, filter=0.5, curve_intensity=1.0},
+			Analog_Turn = {type="joystick_analog", key=4, is_flipped=false, multiplier=1.0, filter=0.05, curve_intensity=1.0},
+			KickerWheel_SetCurrentVelocity = {type="joystick_analog", key=0, is_flipped=false, multiplier=1.0, filter=0.5, curve_intensity=1.0},
 			Joystick_SetCurrentSpeed_2 = {type="joystick_analog", key=1, is_flipped=true, multiplier=1.0, filter=0.05, curve_intensity=0.0},
 			
 			--Robot_SetLowGearOff = {type="joystick_button", key=8, keyboard=';', on_off=false},
---
+++ lua/FRC2015/Competition/Release Version/README.txt
@@ -0,0 +1,3 @@
+FRC2015Robot.lua - has modified values for smoother driving
+
+FRC2015Robot_BACKUP - has only default values for driving, this version is known to work and has been tested, use for debugging or as failsafe.
