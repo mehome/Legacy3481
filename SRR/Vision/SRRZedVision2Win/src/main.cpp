e7efb998f5e277d8cc864b6ad211b23fbc2c4293
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2018-03-02 20:51:32 +0000

    Made localhost SmartDashboard to run as server by default with some comments explaining which to use

--- SRR/Vision/SRRZedVision2Win/src/main.cpp
+++ SRR/Vision/SRRZedVision2Win/src/main.cpp
@@ -214,11 +214,14 @@ int main(int argc, char **argv) {
 	}
 
 	//This app is a client... let SmartDashboard know
-	SmartDashboard::SetClientMode();
 	//For now hard code the IP address, but we may want to pass it as an argument
 	#if 0
+	SmartDashboard::SetClientMode();
 	SmartDashboard::SetIPAddress("10.34.81.99");  //robot
 	#else
+	//Run as a server to test directly to SmartDashboard UI locally
+	//But set as client if testing with a robot server (e.g. robot simulation)
+	//SmartDashboard::SetClientMode();
 	SmartDashboard::SetIPAddress("127.0.0.1");   //localhost
 	#endif
 	SmartDashboard::init();
