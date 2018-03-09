3905558d4a2d35690cb632f60f506bbd5c060633
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2018-03-09 17:21:22 +0000

    Added ability to manipulate the camera mode through SmartDashboard

--- SRR/Vision/SRRZedVision2Win/src/main.cpp
+++ SRR/Vision/SRRZedVision2Win/src/main.cpp
@@ -181,6 +181,72 @@ std::string hook_cascade_name = "bin/data/SRR Samples/cascades/hook_cascade_gpu.
 
 extern cv::CascadeClassifier hook_cascade;
 
+
+//Helper class that will manipulate cam#_op_mode variable through SmartDashboard
+class SmartDashboard_ModeManager
+{
+	private:
+		int &m_CurrentSelection;  //reference to actual variable to manipulate
+		std::string Variable_Name;
+		int m_LastMode; //used as a valve for flood control
+		size_t m_IterationCount=0;  //used to cut down on probing iterations as changes do not need to happen immediately
+	public:
+		SmartDashboard_ModeManager(int &CurrentSelection, const char *SmartName, int DefaultMode) : m_CurrentSelection(CurrentSelection),
+			Variable_Name(SmartName), m_LastMode(DefaultMode)
+		{
+			try
+			{
+				SmartDashboard::GetNumber(SmartName);
+			}
+			catch (...)
+			{
+				const double default_value = (double)DefaultMode;  //too bad they don't support integers yet
+				SmartDashboard::PutNumber(SmartName, default_value);
+			}
+		}
+		//luckily this should have the same changes between either camera
+		void SmartDashboardChanged(bool DestroyMasked)
+		{
+			switch (m_CurrentSelection) 
+			{
+				case 0: printf("mode NONE\n"); break;
+				case 1: printf("mode Find Hook\n"); break;
+				case 2: printf("mode Find Rock\n"); break;
+				case 3: printf("mode Find Beacon\n"); break;
+				case 4: printf("mode passthrough\n"); break;
+			}
+			if (DestroyMasked)
+				cv::destroyWindow("Masked");
+		}
+		void operator()()
+		{
+			const size_t IntervalCount = 60;   //give some time between reads
+			//Will be managing 3 variables... the current smart value, current selection, and last selection
+			//if there is a conflict between the current selection and current smart value where neither are the last... the smart dashboard has priority
+			int current_smart = m_LastMode;
+			if (m_IterationCount++ > IntervalCount)
+			{
+				//probe a new update
+				current_smart = (int)SmartDashboard::GetNumber(Variable_Name.c_str());
+				m_IterationCount = 0;
+			}
+			if ((current_smart != m_CurrentSelection) && (current_smart != m_LastMode))
+			{
+				m_CurrentSelection = current_smart;
+				const bool DestroyMasked = (m_LastMode == 2);  //only if we were finding the rock last time
+				assert((DestroyMasked && m_CurrentSelection != 2) || (!DestroyMasked));  //sanity check
+				m_LastMode = m_CurrentSelection;
+				SmartDashboardChanged(DestroyMasked);  //show change
+			}
+			else if (m_CurrentSelection != m_LastMode) //e.t. changed from keyboard
+			{
+				SmartDashboard::PutNumber(Variable_Name.c_str(), (double)m_CurrentSelection);
+				m_LastMode = m_CurrentSelection;
+			}
+		}
+};
+
+
 //main  function
 //mode 0 = robot
 //mode 1 = simulation
@@ -293,7 +359,7 @@ int main(int argc, char **argv) {
 			cv::Mat frame = FrontCam.GrabFrame();
 
 			if (cam2_op_mode == FindHook)
-				detectHookSample(frame, depth, point_cloud);	// TODO: no point cloud or depth for fromt cam.
+				detectHookSample(frame, depth, point_cloud);	// TODO: no point cloud or depth for front cam.
 			else if (cam2_op_mode == FindRock)
 				ThresholdDet.detectRockSample(frame, depth, point_cloud, SmallWindow);
 			else if (cam2_op_mode == FindBeacon)
@@ -303,7 +369,10 @@ int main(int argc, char **argv) {
 		}
 
 		if (StereoCamEnabled)
-		{	// update
+		{	
+			static SmartDashboard_ModeManager s_SmartDashboard_ModeManager(cam1_op_mode,"ZedMode",cam1_op_mode);
+			s_SmartDashboard_ModeManager();  //update
+			// update
 			if (cam1_op_mode != Idle)
 			{
 				StereoCam.GrabFrameAndDapth();
