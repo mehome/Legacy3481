05bb775fd73c0f4e46e4aa1caaf7ea56eb7b16b8
james <james@4c771cc3-0104-4cfc-9633-31de2bc38340>
2018-10-04 17:13:21 +0000

    Added export support

--- MyProjects/GCodeTools/GCodeTools/GCode_Tools_Internal.cpp
+++ MyProjects/GCodeTools/GCodeTools/GCode_Tools_Internal.cpp
@@ -38,13 +38,13 @@ private:
 	Tabsize m_GlobalSize=Tabsize(0.02, 0.25);  //We can have a global size for all, but keep ability to change individual ones
 	struct TabLocation
 	{
-		TabLocation(size_t LineNumber, double Offset) : m_LineNumber(LineNumber), m_Offset(Offset)
+		TabLocation(size_t LineNumber, double Offset) : m_LineNumber_c(LineNumber), m_Offset(Offset)
 		{}
 		Vec2d m_origin,m_prev_end,m_end_point;
 		double m_Z_depth; //find the last z depth to detemine height relief
 		//The line number is used to compute the origin
 		//The end point is the point far enough away from origin to create tab
-		size_t m_LineNumber;
+		size_t m_LineNumber_c;  //the c means cardinal and so the origin point matches the line number on the text editor
 		double m_Offset;  //how far away from origin (along the contour path) to start tab
 		size_t m_NoLinesForEndPoint;  //Used to compute lines to replace
 	};
@@ -60,7 +60,28 @@ private:
 		{}
 		Tabsize m_properties;
 		TabLocation m_position;
-		GCode m_PatchCode; //this keeps a record of the changes made for the tab from beginning to end (may embed existing gcode)
+		//We make it clear within the patch the intention of what is happening for each line submitted (even if it isn't needed)
+		struct Patch
+		{
+			enum operation
+			{
+				e_op_insert_after_line,
+				e_op_replaced_line
+			};
+			struct PatchLine
+			{
+				PatchLine(text_line _text,operation _lineType,size_t _lineNumber) : text(_text),line_type(_lineType),line_number_c(_lineNumber)
+				{}
+				text_line text;
+				size_t line_number_c;  //cardinal line number the operation pertains to from the source... multiple lines of insertion can point to same line number
+				operation line_type;
+			};
+			void AddLine(const char *text, operation line_type, size_t _lineNumber)
+			{
+				m_PatchLines.push_back(PatchLine(text,line_type, _lineNumber));
+			}
+			std::vector<PatchLine> m_PatchLines;
+		} m_PatchCode; //this keeps a record of the changes made for the tab from beginning to end (may embed existing gcode)
 		size_t m_PatchLinesToReplace;  //A count of how many lines to replace with the patch code
 	};
 	using Tabs = std::vector<Tab>;
@@ -183,7 +204,7 @@ private:
 	}
 	bool ObtainPosition(Tab &tab)
 	{
-		size_t line_index = tab.m_position.m_LineNumber-1;  //line numbers are cardinal... this becomes ordinal
+		size_t line_index = tab.m_position.m_LineNumber_c-1;  //line numbers are cardinal... this becomes ordinal
 		double XValue;
 		double YValue;
 		double ZValue;
@@ -217,11 +238,11 @@ private:
 				tab.m_position.m_prev_end = TestEndPoint;  //need to keep track of previous point to inject a new point
 		}
 		tab.m_position.m_end_point = TestEndPoint;
-		tab.m_position.m_NoLinesForEndPoint = line_index - (tab.m_position.m_LineNumber - 1);
+		tab.m_position.m_NoLinesForEndPoint = line_index - (tab.m_position.m_LineNumber_c - 1);
 		if (GetZ)
 		{
 			//Reset line index to origin but this time going in reverse
-			size_t line_index = tab.m_position.m_LineNumber - 1;  
+			size_t line_index = tab.m_position.m_LineNumber_c - 1;  
 			while ((!ZValueProcessed) && (line_index > 5))
 			{
 				ParseLine_XY(m_GCode[line_index], XValue, YValue, ZValue, ZValueProcessed,true);
@@ -232,7 +253,81 @@ private:
 		}
 		return true;
 	}
+
+	class GCode_Writer
+	{
+	private:
+		const GCode &m_GCode;
+		//where each tab is ordered by the ordinal line number
+		std::map<size_t, const Tab *> m_Tabs;  //using a map to avoid n^2 operation
+		using tab_iter = std::map<size_t, const Tab *>::const_iterator;
+		//We need to maintain the source index pointer as it may not increment for each call
+		size_t m_SourceIndex_o=0;
+		bool m_PatchInFlight = false;
+		size_t m_PatchIndex = 0;
+	public:
+		GCode_Writer(const GCode &_GCode ) : m_GCode(_GCode)
+		{
+		}
+		void AddTabs(const Tabs &_Tabs)
+		{
+			//populate the map
+			for (auto &i : _Tabs)
+				m_Tabs[i.m_position.m_LineNumber_c - 1] = &i;  //insert into ordinal line number positions
+		}
+		//ordinal line number this function expects this to increment with no reason to seek
+		const char *WriteLine(size_t line_number_o, size_t &sourceindex)
+		{
+			const char *ret = nullptr;
+			if (m_SourceIndex_o < m_GCode.size())
+			{
+				//in all cases we try to find a tab line
+				tab_iter Tab = m_Tabs.find(m_SourceIndex_o);
+				if (Tab != m_Tabs.end())
+				{
+					if (!m_PatchInFlight)
+					{
+						m_PatchInFlight = true;
+						m_PatchIndex = 0;
+						//for now assume all operations are insert after this point... so this line gets returned without advancing
+						ret = m_GCode[m_SourceIndex_o].c_str();
+					}
+					else
+					{
+						const Tab::Patch &patch_code = Tab->second->m_PatchCode;
+						//here we deal out the patched code
+						if (m_PatchIndex < patch_code.m_PatchLines.size())
+						{
+							ret = patch_code.m_PatchLines[m_PatchIndex++].text.c_str();
+						}
+						else
+						{
+							//we are done
+							m_SourceIndex_o++;  //advance to next entry
+							//and skip over replaced lines
+							m_SourceIndex_o += Tab->second->m_PatchLinesToReplace;
+							m_PatchInFlight = false;
+						}
+					}
+				}
+				//The usual case
+				if (!m_PatchInFlight)
+					ret = m_GCode[m_SourceIndex_o++].c_str();
+				sourceindex=m_SourceIndex_o;
+			}
+			return ret;
+		}
+		void Flush() 
+		{
+			m_SourceIndex_o = 0;
+			m_PatchInFlight = false;
+			m_PatchIndex = 0;
+			m_Tabs.clear();
+		}
+	} m_GCode_Writer;
 public:
+	Tab_Generator() : m_GCode_Writer(m_GCode)
+	{}
 	bool Load_GCode(const char *filename)
 	{
 		bool ret = false;
@@ -259,7 +354,7 @@ public:
 		}
 		return ret;
 	}
-	void ProcessTab(Tab tab)
+	void ProcessTab(Tab &tab)
 	{
 		//Obtain position from GCode... this will have both points needed to create tab
 		//as well as the current z height
@@ -271,20 +366,57 @@ public:
 		Vec2d SegmentEnd = ContourVector * tab.m_properties.m_width + AnchorPoint;
 		//Now we have everything... time to list it out
 		//starting with first point to be inserted at line number's position
-		printf("X%.4f Y%.4f Z%.4f\n",AnchorPoint[0],AnchorPoint[1],tab.m_position.m_Z_depth+tab.m_properties.m_height);
-		printf("X%.4f Y%.4f\n", SegmentEnd[0], SegmentEnd[1]);
-		printf("X%.4f Y%.4f Z%.4f\n", tab.m_position.m_end_point.x(), tab.m_position.m_end_point.y(), tab.m_position.m_Z_depth);
+		char Buffer[70];
+		sprintf(Buffer,"G2 X%.4f Y%.4f Z%.4f",AnchorPoint[0],AnchorPoint[1],tab.m_position.m_Z_depth+tab.m_properties.m_height);
+		tab.m_PatchCode.AddLine(Buffer, Tab::Patch::e_op_insert_after_line,tab.m_position.m_LineNumber_c);
+		sprintf(Buffer, "G2 X%.4f Y%.4f", SegmentEnd[0], SegmentEnd[1]);
+		tab.m_PatchCode.AddLine(Buffer, Tab::Patch::e_op_insert_after_line, tab.m_position.m_LineNumber_c);
+		sprintf(Buffer, "G2 X%.4f Y%.4f Z%.4f", tab.m_position.m_end_point.x(), tab.m_position.m_end_point.y(), tab.m_position.m_Z_depth);
+		tab.m_PatchCode.AddLine(Buffer, Tab::Patch::e_op_replaced_line, tab.m_position.m_LineNumber_c+tab.m_position.m_NoLinesForEndPoint);
+		tab.m_PatchLinesToReplace = tab.m_position.m_NoLinesForEndPoint;
 		assert(result);  //I intend to make this more robust
 	}
 	//Here is the simplest form to solve tabs
-	void ProcessTab(size_t line_number, double offset = 0.0)
+	Tab ProcessTab(size_t line_number, double offset = 0.0)
 	{
-		ProcessTab(Tab(this, TabLocation(line_number, offset)));
+		Tab NewTab(this, TabLocation(line_number, offset));
+		ProcessTab(NewTab);
+		return NewTab;
+	}
+	bool ExportGCode(const char *filename)
+	{
+		const char *block = nullptr;
+		size_t lineindex = 0;
+		m_GCode_Writer.Flush();  //ensure everything is reset incase we do multiple tries
+		m_GCode_Writer.AddTabs(m_Tabs);
+		size_t SourceIndex=0;
+		if (filename)
+		{
+			std::ofstream out = std::ofstream(filename, std::ios::out);
+			while ((block = m_GCode_Writer.WriteLine(lineindex++, SourceIndex)) != nullptr)
+				out.write(block, strlen(block));
+			out.close();
+		}
+		else
+		{
+			//Note: These post increment so they become cardinal by coicedence when these label the line numbers
+			#if 0
+			while ((block = m_GCode_Writer.WriteLine(lineindex++, SourceIndex)) != nullptr)
+				printf("%d: %s\n", SourceIndex, block);
+			#else
+			while ((block = m_GCode_Writer.WriteLine(lineindex++, SourceIndex)) != nullptr)
+				printf("%d: %s\n", lineindex, block);
+			#endif	
+		}
+		return true;
 	}
 	void Test()
 	{
+		m_GCode.clear();
+		m_Tabs.clear();
 		Load_GCode("TabTest.nc");
-		ProcessTab(41,1.0);
+		m_Tabs.push_back(ProcessTab(41, 1.0));
+		ExportGCode(nullptr);
 	}
 	#if 0
 	void SetOutFilename(const char *filename)
